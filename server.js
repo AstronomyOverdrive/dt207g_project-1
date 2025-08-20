0864ed840a026fd22241e147451be804994a0479
William Pettersson <contact@williampettersson.eu>
2025-08-20 22:05:37 +0200

    Add function to check if order is valid

--- server.js
+++ server.js
@@ -52,19 +52,48 @@ app.get("/public/about", async (req, res) => {
 // Post order
 app.post("/public/order/place", async (req, res) => {
 	try {
-		const dbModel = await mongoose.model("orders", schemas.orderSchema);
 		const newOrder = {
 			items: req.body.items,
 			customerName: req.body.name,
 			customerPhone: req.body.phone,
 			completed: false
 		}
-		const result = await dbModel.create(newOrder);
-		res.status(201).json(result);
+		const errorMsg = await verifyOrder(newOrder);
+		if (errorMsg === "") {
+			const dbModel = await mongoose.model("orders", schemas.orderSchema);
+			const result = await dbModel.create(newOrder);
+			res.status(201).json(result);
+		} else {
+			res.status(400).json({message: errorMsg});
+		}
 	} catch (error) {
 		res.status(500).json({error: "Internal error: " + error});
 	}
 });
+
+async function verifyOrder(order) {
+	let problems = "";
+	// Check if order.items is an array
+	if (order.items instanceof Array) {
+		// Check if array has items
+		if (order.items.length === 0) {
+			problems += "\nOrder kan inte vara tom";
+		} else {
+			// Check if order is of valid menu items
+			const dbModel = await mongoose.model("menuitems", schemas.menuSchema);
+			const menuItems = await dbModel.find({});
+			order.items.forEach(item => {
+				const findItem = menuItems.find(menuItem => String(menuItem._id).includes(item));
+				if (findItem === undefined) {
+					problems += "\nOgiltig produkt";
+				}
+			});
+		}
+	} else {
+		problems += "\nFel format på beställningen";
+	}
+	return problems;
+}
 
 // Start server
 app.listen(process.env.PORT, () => {
