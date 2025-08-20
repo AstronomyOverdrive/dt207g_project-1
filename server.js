cbe9a10baf0ff179ece9d46dba511e815f5e9c94
William Pettersson <contact@williampettersson.eu>
2025-08-20 21:03:25 +0200

    Add GET endpoint for /public/menu

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Import modules
-const mongoose = require('mongoose');
-const schemas = require('./schemas.mjs');
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const schemas = require("./schemas.mjs");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 require("dotenv").config();
 
 // Start app
@@ -16,7 +16,7 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=${process.env.DB_DATABASE}`;
 
 // Database client options
-const clientOptions = {serverApi: {version: '1', strict: true, deprecationErrors: true}};
+const clientOptions = {serverApi: {version: "1", strict: true, deprecationErrors: true}};
 
 // Connect to database
 async function dbConnect() {
@@ -27,6 +27,18 @@ async function dbConnect() {
 		console.log(error);
 	}
 }
+
+// Public routing
+// Get menu
+app.get("/public/menu", async (req, res) => {
+	try {
+		const dbModel = await mongoose.model("menuitems", schemas.menuSchema);
+		const menu = await dbModel.find({});
+		res.status(200).json(menu);
+	} catch (error) {
+		res.status(500).json({error: "Internal error: " + error});
+	}
+});
 
 // Start server
 app.listen(process.env.PORT, () => {
